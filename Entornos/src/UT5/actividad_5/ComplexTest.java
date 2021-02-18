4e37d441d4083d662931c7981437633525639854
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2021-02-18 10:46:01 +0100

    entornos

--- Entornos/src/UT5/actividad_5/ComplexTest.java
+++ Entornos/src/UT5/actividad_5/ComplexTest.java
@@ -1,11 +1,14 @@
 package UT5.actividad_5;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComplexTest {
 
 	@Test
+	@DisplayName ("Suma 1")
 	void testSumar() {
 		Complex prueba1 = new Complex(15, 60);
 		Complex prueba2 = new Complex(95, 30);
@@ -16,47 +19,102 @@ class ComplexTest {
 	}
 
 	@Test
-	void testRestar() {
-		Complex prueba1 = new Complex(15, 60);
-		Complex prueba2 = new Complex(95, 30);
-		
-		Complex resultado = prueba1.restar(prueba2); 
-		Complex r_esperado = new Complex(80, 30);
-		assertEquals(resultado, r_esperado);
-	}
-
-	@Test
+	@DisplayName ("Multiplicacion con numeros complejos 1")
 	void testMultiplicarComplex() {
-		Complex prueba1 = new Complex(15, 60);
-		Complex prueba2 = new Complex(95, 30);
+		Complex prueba1 = new Complex(52, 9);
+		Complex prueba2 = new Complex(30, 6);
 		
 		Complex resultado = prueba1.multiplicar(prueba2); 
-		Complex r_esperado = new Complex(1600, 3000);
+		Complex r_esperado = new Complex(1506, 582);
 		assertEquals(resultado, r_esperado);
 	}
 
 	@Test
+	@DisplayName (" Multiplicacion con numeros dobles 1")
 	void testMultiplicarDouble() {
-		Complex prueba1 = new Complex(35,0);
-		
+		Complex prueba1 = new Complex(95, 30);
 		Complex resultado = prueba1.multiplicar(35); 
-		Complex r_esperado = new Complex(1750, 1050);
+		Complex r_esperado = new Complex(3325, 1050);
 		assertEquals(resultado, r_esperado);
 	}
 
 	@Test
-	void testDividir() {
-		Complex prueba1 = new Complex(50, 0);
-		Complex prueba2 = new Complex(95, 30);
+	@DisplayName ("Division 1")
+	void testDividir1() {
+		Complex prueba1 = new Complex(52, 9);
+		Complex prueba2 = new Complex(30, 6);
 		
 		Complex resultado = prueba1.dividir(prueba2); 
-		Complex r_esperado = new Complex(110, 90);
+		Complex r_esperado = new Complex(1.7243589743589745, -0.04487179487179487);
+		assertEquals(resultado, r_esperado);
+	}
+	@Test
+	@DisplayName ("Division 2")
+	void testDividir2() {
+		Complex prueba1 = new Complex(52, 9);
+		Complex prueba2 = new Complex(30, 6);
+		
+		Complex resultado = prueba1.dividir(prueba2); 
+		Complex r_esperado = new Complex(1.7243589743589745, -0.04487179487179487);
+		assertEquals(resultado, r_esperado);
+	}
+	@Test
+	@DisplayName ("Division 3")
+	void testDividir3() {
+		Complex prueba1 = new Complex(52, 9);
+		Complex prueba2 = new Complex(30, 6);
+		
+		Complex resultado = prueba1.dividir(prueba2); 
+		Complex r_esperado = new Complex(1.7243589743589745, -0.04487179487179487);
+		assertEquals(resultado, r_esperado);
+	}
+	
+	@Test
+	@DisplayName ("Resta 1")
+	void testRestar1() {
+		Complex prueba1 = new Complex(-1, 9);
+		Complex prueba2 = new Complex(-35, 6);
+		
+		Complex resultado = prueba1.restar(prueba2); 
+		Complex r_esperado = new Complex(34, 3);
+		assertEquals(resultado, r_esperado);
+	}
+	@Test
+	@DisplayName ("Resta 2")
+	void testRestar2() {
+		Complex prueba1 = new Complex(0, 35);
+		Complex prueba2 = new Complex(-1, 35);
+		
+		Complex resultado = prueba1.restar(prueba2); 
+		Complex r_esperado = new Complex(1, 0);
+		assertEquals(resultado, r_esperado);
+	}
+	@Test
+	@DisplayName ("Resta 3")
+	void testRestar3() {
+		Complex prueba1 = new Complex();
+		Complex prueba2 = new Complex();
+		
+		Complex resultado = prueba1.restar(prueba2); 
+		Complex r_esperado = new Complex(0,0);
 		assertEquals(resultado, r_esperado);
 	}
 
 	@Test
-	void testEqualsObject() {
-
+	@DisplayName ("Equals 1")
+	void testEqualsObject1() {
+		Complex prueba1 = new Complex(0, 1);
+		Complex prueba2 = new Complex(1, 0);
+		boolean resultado = prueba1.equals(prueba2);
+		assertEquals(resultado, false);
+	}
+	@Test
+	@DisplayName ("Equals 2")
+	void testEqualsObject2() {
+		Complex prueba1 = new Complex(0, 0);
+		Complex prueba2 = new Complex(0, 0);
+		boolean resultado = prueba1.equals(prueba2);
+		assertEquals(resultado, true);
 	}
 
 }
--- Entornos/src/UT5/actividad_5/p_complex.java
+++ Entornos/src/UT5/actividad_5/p_complex.java
@@ -15,13 +15,19 @@ public class p_complex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Complex prueba = new Complex(50, 30);
-		Complex prueba2 = new Complex(20, 5);
+		Complex prueba = new Complex(4, 40);
+		Complex prueba2 = new Complex(2, 2);
+		Complex prueba3 = new Complex(0, 1);
+		Complex prueba4 = new Complex(1, 0);
 		
 		
 		System.out.println(prueba.sumar(prueba2));
-		System.out.println(prueba.multiplicar(prueba));
+		System.out.println(prueba.multiplicar(prueba2));
 		System.out.println(prueba.multiplicar(35));
+		System.out.println(prueba.dividir(prueba2));
+		System.out.println(prueba.restar(prueba2));
+		System.out.println(prueba3.equals(prueba4));
+
 	}
 
 }
